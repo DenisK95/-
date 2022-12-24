cb3ff629d6e763eeb07de00e724b3f0563b1f0bb
Denis <deniskildyakov@gmail.com>
2022-12-24 17:25:03 +0300

    Третий коммит

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿ int a, b;
+ 
+        Console.WriteLine("Введите два числа : ");
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        if (a > b)
+        {
+            Console.WriteLine("наибольшее число", a);
+        }
+    else
+        {
+            Console.WriteLine("наибольшее число ", b);
+        }
+        Console.ReadLine();
+ 
+    
